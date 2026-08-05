c4e14d4c0aa6c0bce095e6e5f9911be2d6157e76
nawafart59-cloud <nawafart59@gmail.com>
2026-08-05 11:11:31 +0700

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyALlSyj16Fv3FuRi5OJaIreh1BFGtLrX28",
+  authDomain: "spesialforyou-7a3d0.firebaseapp.com",
+  projectId: "spesialforyou-7a3d0",
+  storageBucket: "spesialforyou-7a3d0.firebasestorage.app",
+  messagingSenderId: "266013219393",
+  appId: "1:266013219393:web:0466fb066830e1cdf504c0"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/831/831276.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
